bd4702743bb58b3dd67a608a0ddc9017e90858dd
JoaoPauloQA <jopaulomartinsdacostaa@gmail.com>
2025-08-13 15:36:28 -0300

    feat: atualizar fixtures de login para e-mails inválidos

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -33,108 +33,108 @@ describe('login', function () {
 
 
 
-context('quando o usuario é bom mas a senha esta incorreta', function () { 
+    context('quando o usuario é bom mas a senha esta incorreta', function () {
 
-    
 
-    before(function () {
 
-         cy.fixture('login').then(function (login) {
+        before(function () {
+
+            cy.fixture('login').then(function (login) {
 
                 this.login = login.login
 
-            cy.postUser(this.login).then(function () {
+                cy.postUser(this.login).then(function () {
 
-                this.login.password = 'abc123'
-            
-        })
+                    this.login.password = 'abc123'
 
-    })
-    })
-
-    it('deve notificar erro de credenciais', function () {
-
-        loginPage.go()
-        loginPage.form(this.login)
-        loginPage.submit()
-
-
-        const message = 'Ocorreu um erro ao fazer login, verifique suas credenciais.'
-        loginPage.toast.shouldHaveTest(message)
-
-    })
-
-
-    context('quando o formato do email é invalido', function () {
-
-        const emails = [
-
-            'papipto.com.br',
-            'joao123',
-            '@',
-            'xpto56',
-            '111',
-            '~~dasd',
-            '74637',
-            '@gmail.com'
-
-        ]
-
-        before(function () {
-
-            loginPage.go()
-
-        })
-
-        emails.forEach(function (email) {
-            it('nao deve logar com email:' + email, function () {
-                const user = {
-
-                    email: email, password: 'pwd1234'
-                }
-
-
-                loginPage.form(user)
-                loginPage.submit()
-                loginPage.alert.HaveText('Informe um email válido')
+                })
 
             })
-
         })
 
+        it('deve notificar erro de credenciais', function () {
 
-
-    })
-
-
-    context('Quando não preencho nenhum dos campos', function () {
-
-        const alertMessages = [
-
-            'E-mail é obrigatório',
-            'Senha é obrigatória',
-
-        ]
-
-        before(function () {
             loginPage.go()
+            loginPage.form(this.login)
             loginPage.submit()
 
+
+            const message = 'Ocorreu um erro ao fazer login, verifique suas credenciais.'
+            loginPage.toast.shouldHaveTest(message)
+
         })
 
-        alertMessages.forEach(function (alert) {
 
-            it('deve exibir ' + alert.toLowerCase(), function () {
+        context('quando o formato do email é invalido', function () {
 
-                loginPage.alert.HaveText(alert)
+
+
+            before(function () {
+
+                cy.fixture('login').then(function (login) {
+
+                    this.invalid_emails = login.invalid_emails
+                })
+
+                loginPage.go()
+
+            })
+
+
+
+
+            it('não deve logar com email:' , function () {
+
+                this.invalid_emails.forEach(function (email) {
+
+                    const user = {
+
+                        email: email, password: 'pwd1234'
+                    }
+
+
+                    loginPage.form(user)
+                    loginPage.submit()
+                    loginPage.alert.HaveText('Informe um email válido')
+
+                })
 
 
             })
 
+
+
         })
 
 
-    })
+        context('Quando não preencho nenhum dos campos', function () {
 
-})
+            const alertMessages = [
+
+                'E-mail é obrigatório',
+                'Senha é obrigatória',
+
+            ]
+
+            before(function () {
+                loginPage.go()
+                loginPage.submit()
+
+            })
+
+            alertMessages.forEach(function (alert) {
+
+                it('deve exibir ' + alert.toLowerCase(), function () {
+
+                    loginPage.alert.HaveText(alert)
+
+
+                })
+
+            })
+
+
+        })
+
+    })
 })
